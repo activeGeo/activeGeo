4d8d8a17dc4572afeae1de653e0bb3e63446e332
activeGeo <hongal20@mails.tsinghua.edu.cn>
2023-03-02 07:29:03 +0000

    fix lg filter_by_template bugs

--- 1_looking_glass/code/1_filter_by_template/run.py
+++ 1_looking_glass/code/1_filter_by_template/run.py
@@ -3,10 +3,12 @@ import os
 SRC_WEBSITES_PATH = os.path.realpath('../../src/websites.txt')
 EACH_TEMPLATE_DIR = './result'
 
-TEMPLATE_0_DIR = f'{EACH_TEMPLATE_DIR}/template_0'
-os.system(f'mkdir -p {TEMPLATE_0_DIR}')
-os.system(f'ln -sf {SRC_WEBSITES_PATH} {TEMPLATE_0_DIR}/bad_websites.txt')
+# TEMPLATE_0_DIR = f'{EACH_TEMPLATE_DIR}/template_0'
+# os.system(f'mkdir -p {TEMPLATE_0_DIR}')
+# os.system(f'ln -sf {SRC_WEBSITES_PATH} {TEMPLATE_0_DIR}/bad_websites.txt')
 
-for i in range(6, 7):
+TEMPLATE_NUM = len([x for x in os.listdir('.') if x.startswith('template')])
+
+for i in range(0, TEMPLATE_NUM+1):
     NOW_RESULT_DIR = f'./result/template_{i-1}'
     os.system(f'python3 ./template_{i}.py {NOW_RESULT_DIR}/bad_websites.txt {i}')
